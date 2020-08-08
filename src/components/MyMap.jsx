8293efb80ed3b672bdcd76cd47214137688c2b1d
CodingWith-Adam <adam@example.com>
2020-08-08 12:42:38 -0600

    Complete project

---
+++ src/components/MyMap.jsx
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+import { Map, GeoJSON } from "react-leaflet";
+import mapData from "./../data/countries.json";
+import "leaflet/dist/leaflet.css";
+import "./MyMap.css";
+
+class MyMap extends Component {
+  state = { color: "#ffff00" };
+
+  color = ["green", "blue", "yellow", "orange", "grey"];
+
+  componentDidMount() {
+    console.log(mapData);
+  }
+
+  countryStyle = {
+    fillColor: "red",
+    fillOpacity: 1,
+    color: "black",
+    weight: 2,
+  };
+
+  printMesssageToConsole = (event) => {
+    console.log("Clicked");
+  };
+
+  changeCountryColor = (event) => {
+    event.target.setStyle({
+      color: "green",
+      fillColor: this.state.color,
+      fillOpacity: 1,
+    });
+  };
+
+  onEachCountry = (country, layer) => {
+    const countryName = country.properties.ADMIN;
+    console.log(countryName);
+    layer.bindPopup(countryName);
+
+    layer.options.fillOpacity = Math.random(); //0-1 (0.1, 0.2, 0.3)
+    // const colorIndex = Math.floor(Math.random() * this.color.length);
+    // layer.options.fillColor = this.color[colorIndex]; //0
+
+    layer.on({
+      click: this.changeCountryColor,
+    });
+  };
+
+  colorChange = (event) => {
+    this.setState({ color: event.target.value });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1 style={{ textAlign: "center" }}>My Map</h1>
+        <Map style={{ height: "80vh" }} zoom={2} center={[20, 100]}>
+          <GeoJSON
+            style={this.countryStyle}
+            data={mapData.features}
+            onEachFeature={this.onEachCountry}
+          />
+        </Map>
+        <input
+          type="color"
+          value={this.state.color}
+          onChange={this.colorChange}
+        />
+      </div>
+    );
+  }
+}
+
+export default MyMap;
